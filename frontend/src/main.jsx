c2cd1455678fc1ea397135381c7ea16deb381213
bisre1921 <bisrattewodros3@gmail.com>
2024-07-25 12:14:23 +0300

    create ProductDetail page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,13 @@ import "./assets/styles/bootstrap.custom.css"
 import "./assets/styles/index.css"
 import {createBrowserRouter , createRoutesFromElements, Route , RouterProvider} from "react-router-dom"
 import Home from './pages/Home.jsx'
+import ProductDetail from './pages/ProductDetail.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path="/" element={<Home />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path='/product/:id' element={<ProductDetail />} />
     </Route>
   )
 )
