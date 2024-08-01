175162dd7ed109c875e6828790be8b862bd00491
bisre1921 <bisrattewodros3@gmail.com>
2024-08-01 20:05:51 +0300

    products API slice and get products endpoint

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,30 +2,51 @@ import React, { useEffect, useState } from 'react'
 import {Row , Col} from 'react-bootstrap'
 // import products from "../products"
 import Product from "../components/Product"
-import axios from "axios"
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+// import axios from "axios"
 
 const Home = () => {
-  const [products , setProducts] = useState([])
+  const {data: products , isLoading , error} = useGetProductsQuery()
+  // const [products , setProducts] = useState([])
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  } , [])
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get("/api/products");
+  //     setProducts(data);
+  //   };
+  //   fetchProducts();
+  // } , [])
 
   return (
     <>
-        <h1>Latest Products</h1>
+      {/* {isLoading && <h2>Loading...</h2>}
+      {isError && <h2>Error</h2>} */}
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>
+          {error?.data?.message || error?.error}
+        </div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+              {products?.map(product => (
+                  <Col key={product.name} sm={12} md={6} lg={4} xl={3}>
+                      <Product product={product} />
+                  </Col>
+              )) }
+          </Row>
+        </>
+      )}
+        {/* <h1>Latest Products</h1>
         <Row>
             {products?.map(product => (
                 <Col key={product.name} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
                 </Col>
             )) }
-        </Row>
+        </Row> */}
     </>
   )
 }
