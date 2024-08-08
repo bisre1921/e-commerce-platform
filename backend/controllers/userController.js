8a83bcf9e894b07d152e3747553c55fda0c9ad99
bisre1921 <bisrattewodros3@gmail.com>
2024-08-08 15:48:07 +0300

    setup JWT Httponly cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,20 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email : email})
 
     if(user && (await user.matchPassword(password))) {
+        const token = jwt.sign({userId: user._id} , process.env.JWT_SECRET , {expiresIn: '30d'})
+
+        res.cookie("jwt" , token , {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            maxAge: 30 * 24 * 60 * 60 * 1000
+        })
         res.json({
             _id: user._id,
             name: user.name,
